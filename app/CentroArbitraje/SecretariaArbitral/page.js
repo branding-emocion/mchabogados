c423175fb7d1bc4bec1ba332be283ffc12eb48b8
jhonned01 <jhonned01@gmail.com>
2025-09-30 18:10:52 -0500

    hover

--- app/CentroArbitraje/SecretariaArbitral/page.js
+++ app/CentroArbitraje/SecretariaArbitral/page.js
@@ -56,14 +56,23 @@ export default function SecretariaArbitral() {
                 </p>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 ">
-                  <Button href="/pdfs/SECRETARIOGENERAL.pdf">
+                  <Button
+                    href="/pdfs/SECRETARIOGENERAL.pdf"
+                    className={"hover:cursor-pointer"}
+                  >
                     Secretario General{" "}
                   </Button>
 
-                  <Button href="/pdfs/EduardoAdolfo.pdf">
+                  <Button
+                    href="/pdfs/EduardoAdolfo.pdf"
+                    className={"hover:cursor-pointer"}
+                  >
                     Secretario Arbitral
                   </Button>
-                  <Button href="/pdfs/JuanArmando.pdf">
+                  <Button
+                    href="/pdfs/JuanArmando.pdf"
+                    className={"hover:cursor-pointer"}
+                  >
                     {" "}
                     Secretario Arbitral
                   </Button>
